31ff08d9bcabb45a283b6a3609dbee86aaa7f4d2
casalecchi <felipevilela191@gmail.com>
2024-04-06 16:36:19 -0300

    TDD: write some tests

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -19,7 +19,15 @@ final class BullseyeTests: XCTestCase {
         game = nil
     }
 
-    func testExample() throws {
-        XCTAssertEqual(game.points(guessValue: 50), 999)
+    func testScorePositive() {
+        var guess = game.target + 5
+        var score = game.points(guessValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+    func testScoreNegative() {
+        var guess = game.target - 5
+        var score = game.points(guessValue: guess)
+        XCTAssertEqual(score, 95)
     }
 }
