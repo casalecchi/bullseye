03136ceefe3f7d02ce242ddd2f4cde65047c2d4e
casalecchi <felipevilela191@gmail.com>
2024-04-07 16:11:31 -0300

    lesson 5 completed

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -13,43 +13,65 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .bold()
-                .multilineTextAlignment(.center)
-                .lineSpacing(4)
-                .font(.footnote)
-                .kerning(2)
-            Text(String(game.target))
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .kerning(-1)
-            HStack {
-                Text("1")
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack {
+                Text("🎯🎯🎯\nPut the bullseye as close as you can to".uppercased())
                     .bold()
-                Slider(value: $value, in: 1.0...100.0)
-                Text("100")
-                    .bold()
-            }
-            Button("Hit me") {
-                isAlertVisible = true
-            }
-            .alert(
-                "Hello there!",
-                isPresented: $isAlertVisible,
-                actions: {
-                    Button("Testing...") { 
-                        value = 50
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4)
+                    .font(.footnote)
+                    .kerning(2)
+                    .padding(.horizontal, 30)
+                    .foregroundStyle(Color("TextColor"))
+                Text(String(game.target))
+                    .font(.largeTitle)
+                    .foregroundStyle(Color("TextColor"))
+                    .fontWeight(.black)
+                    .kerning(-1)
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: $value, in: 1.0...100.0)
+                    Text("100")
+                        .bold()
+                }
+                .foregroundStyle(Color("TextColor"))
+                .padding()
+                Button("Hit me".uppercased()) {
+                    isAlertVisible = true
+                }
+                .padding(20)
+                .background(
+                    ZStack {
+                        Color("ButtonColor")
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
                     }
-                },
-                message: {
-                    let roundedValue = Int(value.rounded())
-                    Text("Your target: \(roundedValue).\nYou scored \(game.points(for: roundedValue)) points")
-                })
+                )
+                .foregroundStyle(.white)
+                .cornerRadius(21)
+                .bold()
+                .font(.title3)
+                .kerning(1)
+                .alert(
+                    "Hello there!",
+                    isPresented: $isAlertVisible,
+                    actions: {
+                        Button("Testing...") {
+                            value = 50
+                        }
+                    },
+                    message: {
+                        let roundedValue = Int(value.rounded())
+                        Text("Your target: \(roundedValue).\nYou scored \(game.points(for: roundedValue)) points")
+                    })
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
